99fb64b2ee9c90fee24053e47235aeba8f2885c7
Marci Vallario <marcivallario@gmail.com>
2021-12-02 16:28:30 -0500

    commit tree 87d3e4ed0cc4a084e581d32f4a35e65735422f2d

--- src/index.js
+++ src/index.js
@@ -1,1 +1,48 @@
 // write your code here
+let ramenMenu;
+const ramenMenuList = document.getElementById('ramen-menu');
+const newRamenForm = document.getElementById('new-ramen');
+
+document.addEventListener('DOMContentLoaded', () => {
+    fetch('http://localhost:3000/ramens')
+    .then(resp => resp.json())
+    .then(ramenDishes => {
+        ramenMenu = ramenDishes;
+        ramenMenu.forEach(dish => loadRamen(dish))
+    })
+
+    newRamenForm.addEventListener('submit', event => {
+        event.preventDefault();
+        const newRamen = {
+            name: event.target.name.value,
+            restaurant: event.target.restaurant.value,
+            image: event.target.image.value,
+            rating: event.target.rating.value,
+            comment: event.target["new-comment"].value
+        }
+        loadRamen(newRamen);
+    })
+})
+
+function loadRamen(ramenDish) {
+    let ramenImg = document.createElement('img');
+    ramenImg.addEventListener('click', () => displayDish(ramenDish))
+    ramenImg.src = ramenDish.image;
+    ramenMenuList.append(ramenImg);
+}
+
+function displayDish(ramenDish) {
+    const detailDiv = document.getElementById('ramen-detail');
+    const detailImg = detailDiv.querySelector('img');
+    const detailName = detailDiv.querySelector('h2');
+    const detailRestaurant = detailDiv.querySelector('h3');
+    let rating = document.getElementById('rating-display')
+    let comment = document.getElementById('comment-display');
+
+
+    detailImg.src = ramenDish.image;
+    detailName.textContent = ramenDish.name;
+    detailRestaurant.textContent = ramenDish.restaurant
+    rating.textContent = ramenDish.rating;
+    comment.textContent = ramenDish.comment;
+}
